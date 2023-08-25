724d47927e407f2f9b7a67a2455542158251615a
Oleksandr Havrilutsa <alex.mercer.324110@gmail.com>
2023-08-25 13:26:00 +0300

    ok

--- 5 Space Invaders/src/main.ts
+++ 5 Space Invaders/src/main.ts
@@ -1,11 +1,1 @@
-import p5 from 'p5'
-
-export const p: p5 = new p5((p: p5) => {
-	p.setup = () => {
-		p.createCanvas(window.innerWidth, window.innerHeight - 1)
-	}
-
-	p.draw = () => {
-		p.background('#202124')
-	}
-})
+import './sketch'
--- 5 Space Invaders/src/sketch.ts
+++ 5 Space Invaders/src/sketch.ts
@@ -1,22 +1,12 @@
 import p5 from 'p5'
-import Drop from './Drop'
-
-let drops: Drop[] = Array.from({ length: 500 })
 
 const sketch = (p: p5) => {
 	p.setup = () => {
 		p.createCanvas(window.innerWidth, window.innerHeight - 1)
-
-		drops = drops.map(() => new Drop())
 	}
 
 	p.draw = () => {
 		p.background('#202124')
-
-		for (let i = 0; i < drops.length; i++) {
-			drops[i].fall()
-			drops[i].show()
-		}
 	}
 }
 
