0704945f106cbf575206d915bc949c485076a544
Oleksandr Havrylutsa <alex.mercer.324110@gmail.com>
2023-08-26 08:37:02 +0300

    ok

--- 5 Space Invaders/src/sketch.ts
+++ 5 Space Invaders/src/sketch.ts
@@ -2,11 +2,11 @@ import p5 from 'p5'
 
 const sketch = (p: p5) => {
 	p.setup = () => {
-		p.createCanvas(window.innerWidth, window.innerHeight - 1)
+		p.createCanvas(window.innerWidth, window.innerHeight)
 	}
 
 	p.draw = () => {
-		p.background('#202124')
+		p.background('white')
 	}
 }
 
